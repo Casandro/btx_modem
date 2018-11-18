7228c72eea86bdfeb988d858535de32d2da739a3
root <root@trashcan.clarke-3.de>
2018-11-18 15:04:52 +0100

    Added code for arrow

--- tools/demod/decode.sh
+++ tools/demod/decode.sh
@@ -3,12 +3,14 @@
 file=$1
 frq=$2
 rate=$3
-
+arrow=$4
 
 rate1=$(($frq*4))
 rate2=$(($frq*2))
 rate3=$(($rate*16))
 
-echo $rate1 $rate2 $rate3
+gcc demod.c -o demod
+gcc uart.c -o uart
 
-sox $file -r $(($frq*4)) -t dat - | tail -n +3 | ./demod | sox -t dat -r $rate2 -c 1 - -r $rate3 -t dat - sinc 0-$rate | less #tail -n +3 | ./uart
+
+sox $file -r $(($frq*4)) -t dat - | tail -n +3 | ./demod | sox -t dat -r $rate2 -c 1 - -r $rate3 -t dat - sinc 0-$rate | tail -n +3 | ./uart $arrow
